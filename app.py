fef1fed3c54902abce00641467bdac9ed36273c6
Keertika Palaniappan <keertikamanikandan@gmail.com>
2026-07-06 13:04:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -202,10 +202,16 @@ rag_bot = ContextChatEngine(
 
 
 # ---------------- HEADER ----------------
-col1, col2, col3 = st.columns([2, 1, 2])
+st.markdown(
+    """
+    <div style="text-align:center;">
+    """,
+    unsafe_allow_html=True
+)
 
-with col2:
-    st.image("image/logo.jpg", width=220)
+st.image("image/logo.jpg", width=220)
+
+st.markdown("</div>", unsafe_allow_html=True)
 
 st.markdown("<div class='main-title'>CarePulse AI</div>", unsafe_allow_html=True)
 st.markdown("<div class='sub-title'>CarePulse Health Assistant</div>", unsafe_allow_html=True)
